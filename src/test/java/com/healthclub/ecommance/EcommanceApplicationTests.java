196cc4d6c12a02796f16147a77b4e49421dc539b
Arvin_Hu <arvin_hu@Arvin-HudeMacBook-Pro.local>
2021-03-12 14:20:58 +0800

    Initial commit

---
+++ src/test/java/com/healthclub/ecommance/EcommanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.healthclub.ecommance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommanceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
